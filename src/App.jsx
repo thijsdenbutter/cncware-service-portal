edc429d2314e6624f2711c6e0eebf8db1154f63f
thijsdenbutter <thijs@cncware.nl>
2025-03-27 22:26:11 +0100

    fout melding opgelost door login te importeren

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/home/Home.jsx";
 import Chats from "./pages/chats/Chats.jsx";
 import NewChat from "./pages/newChat/NewChat.jsx";
+import Login from "./pages/login/Login.jsx";
+import PageNotFound from "./pages/pageNotFound/PageNotFound.jsx";
 
 function App() {
 
   return (
     <>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home/>}/>
             <Route path="/chats" element={<Chats />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/new-chats" element={<NewChat />} />
+            <Route path="/new-chat" element={<NewChat />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     </>
   )
