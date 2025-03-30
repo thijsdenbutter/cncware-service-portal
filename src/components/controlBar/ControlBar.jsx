369b450fd533b6d79c2b30e1a61a63d1a7f61d34
thijsdenbutter <thijs@cncware.nl>
2025-03-30 12:07:39 +0200

    control bar volgorder aangepast voor rechter controls

--- src/components/controlBar/ControlBar.jsx
+++ src/components/controlBar/ControlBar.jsx
@@ -1,11 +1,19 @@
 import './ControlBar.css'
+import {NavLink} from "react-router-dom";
 
 function ControlBar() {
     return (
         <div className="control-bar-outer-container">
-            <button>button1</button>
-            <button>button2</button>
+            <div className="control-bar-inner-container">
+                <button>button1</button>
+                <button>button2</button>
+            </div>
+            <div className="control-bar-inner-container">
+                <NavLink to="/new-chat">New chat</NavLink>
+                <button>Stop</button>
+            </div>
         </div>
     )
 }
+
 export default ControlBar;
