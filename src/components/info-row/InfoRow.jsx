d853ab638a1205e78cbc7742ca5bcbfeebf7a55a
thijsdenbutter <thijs@cncware.nl>
2025-03-31 22:23:47 +0200

    semantische element gewijzigd naar p

--- src/components/info-row/InfoRow.jsx
+++ src/components/info-row/InfoRow.jsx
@@ -3,7 +3,7 @@ import './InfoRow.css'
 function InfoRow({label, value}) {
     return (
         <div className='info-row'>
-            <h2 className="info-label">{label}</h2>
+            <p className="info-label">{label}</p>
             <p className="info-value">{value}</p>
         </div>
     )
