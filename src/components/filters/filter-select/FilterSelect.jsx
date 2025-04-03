73af4e04630b1dc7d2164c7b86d2c08dd8daa1df
thijsdenbutter <thijs@cncware.nl>
2025-04-03 21:40:23 +0200

    filter tekst kleur veranderd voor placeholder status

--- src/components/filters/filter-select/FilterSelect.jsx
+++ src/components/filters/filter-select/FilterSelect.jsx
@@ -2,7 +2,10 @@ import './FilterSelect.css'
 
 function FilterSelect({ value, onChange, options, placeholder }) {
     return (
-        <select className="filter-input" value={value} onChange={onChange}>
+        <select
+            className={`filter-input ${value === "" ? "placeholder-active" : ""}`}
+            value={value}
+            onChange={onChange}>
             <option value="">{placeholder}</option>
             {options.map((option) => (
                 <option key={option} value={option}>
