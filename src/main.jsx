3cdfda1c8ef5db9b396055f0484f7ec4f24e7dc2
thijsdenbutter <thijs@cncware.nl>
2025-04-07 20:54:08 +0200

    stickmode weggehaald na to many request error

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,13 +6,11 @@ import {FilterProvider} from "./context/FilterContext.jsx";
 import {AuthProvider} from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <AuthProvider>
-            <Router>
-                <FilterProvider>
-                    <App/>
-                </FilterProvider>
-            </Router>
-        </AuthProvider>
-    </StrictMode>,
+    <AuthProvider>
+        <Router>
+            <FilterProvider>
+                <App/>
+            </FilterProvider>
+        </Router>
+    </AuthProvider>
 )
