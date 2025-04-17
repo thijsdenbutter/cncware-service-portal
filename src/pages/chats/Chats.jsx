a7b5e33a6558b35dcb38ef39aee51c00c930df2b
thijsdenbutter <thijs@cncware.nl>
2025-04-17 11:43:52 +0200

    statussen ophalen met openenen van chats pagina

--- src/pages/chats/Chats.jsx
+++ src/pages/chats/Chats.jsx
@@ -1,14 +1,23 @@
-import './Chats.css'
+import './Chats.css';
 import ChatBar from "../../components/chats/chat-bar/ChatBar.jsx";
 import Chat from "../../components/chats/chat/Chat.jsx";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {TimerContext} from "../../context/TimerContext.jsx";
+import {TeamleaderContext} from "../../context/TeamleaderContext.jsx";
 
 function Chats() {
 
     const {
         selectedChat
     } = useContext(TimerContext);
+
+    const {
+        fetchTicketStatuses
+    } = useContext(TeamleaderContext);
+
+    useEffect(() => {
+        fetchTicketStatuses();
+    },[]);
 
     return (
         <div className="chats-layout">
@@ -17,7 +26,7 @@ function Chats() {
                 <Chat/>
             )}
         </div>
-    )
+    );
 }
 
 export default Chats;
